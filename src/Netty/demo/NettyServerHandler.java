45abe746fe2c0275afba9d05e2de2f0b8a2ce623
吴浩 <wuhaodeyx@163.com>
2018-07-29 23:05:49 +0800

    添加函数注解

--- src/Netty/demo/NettyServerHandler.java
+++ src/Netty/demo/NettyServerHandler.java
@@ -18,9 +18,7 @@ public class NettyServerHandler extends ChannelHandlerAdapter {
 	 * @param msg
 	 */
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
-		
 		ByteBuf buf = (ByteBuf) msg;
-		
 		String received = getMessage(buf);
 		System.out.println("服务器接收到消息：" + received);
 		
@@ -36,8 +34,9 @@ public class NettyServerHandler extends ChannelHandlerAdapter {
 	 * 从ByteBuf中获取信息 使用UTF-8编码返回
 	 */
 	private String getMessage(ByteBuf buf) {
-
+		// readableBytes():返回可读的字节数 writableBytes():返回可以写入的字节数
 		byte[] con = new byte[buf.readableBytes()];
+		// 将此缓冲区的数据传输到新创建的缓冲区
 		buf.readBytes(con);
 		try {
 			return new String(con, "UTF-8");
@@ -51,7 +50,13 @@ public class NettyServerHandler extends ChannelHandlerAdapter {
 			throws UnsupportedEncodingException {
 
 		byte[] req = message.getBytes("UTF-8");
+		/**
+		 * buffer:创建一个具有相当小的初始容量的新的大型Java堆缓冲区，它可以根据需求无限制地扩展其容量
+		 * copiedBuffer(ByteBuf buffer):创建一个新的缓冲区，其内容是指定缓冲区可读字节的副本
+		 */
+
 		ByteBuf pingMessage = Unpooled.buffer();
+		// writeBytes(byte[] src):将指定的源数组的数据(src)传输到从当前writerIndex开始的缓冲区，并通过传输的字节数增加writerIndex
 		pingMessage.writeBytes(req);
 		return pingMessage;
 	}
