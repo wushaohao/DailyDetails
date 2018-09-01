2444d049153a1f16528fe818286bbf6d3cf79bd3
吴浩 <wuhaodeyx@163.com>
2018-09-01 18:07:33 +0800

    基于ReentrantLock实现线程间隔打印(交通灯)

--- src/threadpools/thread/IntervalPrint2.java
+++ src/threadpools/thread/IntervalPrint2.java
@@ -6,7 +6,7 @@ import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @author:wuhao
- * @description:间隔打印(ReentrantLock)
+ * @description:3个间隔打印(ReentrantLock实现交通灯信号)
  * @date:18/9/1
  */
 public class IntervalPrint2 implements Runnable{
@@ -20,6 +20,7 @@ public class IntervalPrint2 implements Runnable{
 
     private  Condition redCon = lock.newCondition();
     private  Condition greenCon = lock.newCondition();
+    private  Condition yellowCon = lock.newCondition();
 
     public static void main(String[] args) {
         new Thread(new IntervalPrint2()).start();
@@ -40,6 +41,7 @@ public class IntervalPrint2 implements Runnable{
     public void run() {
         new Thread(new RedLight(), "red light").start();
         new Thread(new GreenLight(), "green light").start();
+        new Thread(new YellowLight(), "yellow light").start();
     }
 
     class RedLight implements Runnable {
@@ -102,6 +104,45 @@ public class IntervalPrint2 implements Runnable{
                 while (tNum != 2) {
                     try {
                         greenCon.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                System.out.println(Thread.currentThread().getName()+ " is flashing...");
+                // 停留时间，便于从控制台观看
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                    tNum = 3;
+                    yellowCon.signal();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+
+    class YellowLight implements Runnable {
+        /**
+         * When an object implementing interface <code>Runnable</code> is used
+         * to create a thread, starting the thread causes the object's
+         * <code>run</code> method to be called in that separately executing
+         * thread.
+         * <p>
+         * The general contract of the method <code>run</code> is that it may
+         * take any action whatsoever.
+         *
+         * @see Thread#run()
+         */
+        @Override
+        public void run() {
+            while (true) {
+                lock.lock();
+
+                while (tNum != 3) {
+                    try {
+                        yellowCon.await();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
