2d50b0b13b0e66147fb07e2d1a73787845ea2f01
吴浩 <wuhaodeyx@163.com>
2019-04-08 10:53:25 +0800

    自定义可重入锁

---
+++ src/main/java/lock/define/ReLock.java
@@ -0,0 +1,32 @@
+package lock.define;
+
+/**
+ * @author:wuhao
+ * @description:自定义可重入锁
+ * @date:2019/4/4
+ */
+public class ReLock {
+    boolean isLocked = false;
+    Thread lockedBy = null;
+    int lockedCount = 0;
+
+    public synchronized void lock() throws InterruptedException {
+        Thread thread = Thread.currentThread();
+        while (isLocked && lockedBy != thread) {
+            wait();
+        }
+        isLocked = true;
+        lockedCount++;
+        lockedBy = thread;
+    }
+
+    public synchronized void unlock() {
+        if (Thread.currentThread() == this.lockedBy) {
+            lockedCount--;
+            if (lockedCount == 0) {
+                isLocked = false;
+                notify();
+            }
+        }
+    }
+}
